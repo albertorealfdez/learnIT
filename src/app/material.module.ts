2881143b82decdb75ab13997f9ea8bcbdf4d8af0
Arf291 <albertorealfdez@gmail.com>
2017-11-13 21:05:09 +0100

    Set basic material design for home page

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatToolbarModule, MatSidenavModule, MatIconModule  } from '@angular/material';
+import { MatButtonModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatInputModule, MatFormFieldModule  } from '@angular/material';
 
 @NgModule({
-  imports: [ MatButtonModule, MatToolbarModule, MatSidenavModule, MatIconModule ],
-  exports: [ MatButtonModule, MatToolbarModule, MatSidenavModule, MatIconModule ],
+  imports: [ MatButtonModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatInputModule, MatFormFieldModule ],
+  exports: [ MatButtonModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatInputModule, MatFormFieldModule ],
 })
 
 export class MaterialModule { }
