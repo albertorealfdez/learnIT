1c40a8980d2da59c140642a637db4551f45c4fe6
Arf291 <albertorealfdez@gmail.com>
2017-12-03 13:27:19 +0100

    Set express server and Heroku config

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, './dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './dist/index.html'));
+});
+
+const port = process.env.APP_PORT || 8000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('Running'));
