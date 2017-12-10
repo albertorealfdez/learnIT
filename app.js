f894e10c999d56efbdf5cc44c1e7f37e2c712974
Arf291 <albertorealfdez@gmail.com>
2017-12-10 19:28:39 +0100

    Changed env PORT variable

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './dist/index.html'));
 });
 
-const port = process.env.APP_PORT || 8000;
+const port = process.env.PORT || 8000;
 app.set('port', port);
 
 const server = http.createServer(app);
