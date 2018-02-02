fa754039c4f3a095df5b67b2b32725fa7b3b350f
Arf291 <albertorealfdez@gmail.com>
2018-02-02 18:36:57 +0100

    Added arrow marker for competences connections

--- src/app/course/student-map/student-map.component.ts
+++ src/app/course/student-map/student-map.component.ts
@@ -37,66 +37,79 @@ export class StudentMapComponent implements OnInit, AfterViewInit {
     
   }
   ngAfterViewInit(){
-    this.svg = d3.select("svg");
+    this.svg = d3.select('svg');
     
-    var width = +this.svg.attr("width");
-    var height = +this.svg.attr("height");
+    var width = +this.svg.attr('width');
+    var height = +this.svg.attr('height');
 
     this.color = d3.scaleOrdinal(d3.schemeCategory20);
     
     this.simulation = d3.forceSimulation()
-        .force("link", d3.forceLink().id(function(d: any) { return d._id; }))
-        .force("charge", d3.forceManyBody())
-        .force("center", d3.forceCenter(width / 2, height / 2));
+        .force('link', d3.forceLink().id(function(d: any) { return d._id; }))
+        .force('charge', d3.forceManyBody())
+        .force('center', d3.forceCenter(width / 2, height / 2));
 
     // Temporal
     this.studentMap.connections = [
-      { "source": "5a2adaab975a6d4285359f0a", "target": "5a2adaab975a6d4285359f0b"}
+      { 'source': '5a2adaab975a6d4285359f0a', 'target': '5a2adaab975a6d4285359f0b'}
     ];
     this.render();
   }
 
   ticked() {
     this.link
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+        .attr('x1', (d) => { return d.source.x; })
+        .attr('y1', (d) => { return d.source.y; })
+        .attr('x2', (d) => { return d.target.x; })
+        .attr('y2', (d) => { return d.target.y; });
 
     this.node
-        .attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
+        .attr('cx', function(d) { return d.x; })
+        .attr('cy', function(d) { return d.y; });
   }
   
   public render() {
-    this.link = this.svg.append("g")
-    .attr("class", "links")
-    .selectAll("line")
+    this.link = this.svg.append('g')
+    .attr('class', 'links')
+    .selectAll('line')
     .data(this.studentMap.connections)
-    .enter().append("line")
-      .attr("stroke-width", 2)
-      .attr("stroke", "black");
+    .enter()
+      .append('line')
+        .attr('stroke-width', 5)
+        .attr('stroke', 'black')
+        .attr('marker-end', 'url(#end-arrow)');
     
-    this.node = this.svg.append("g")
-      .attr("class", "nodes")
-      .selectAll("circle")
+    this.node = this.svg.append('g')
+      .attr('class', 'nodes')
+      .selectAll('circle')
       .data(this.studentMap.competences)
-      .enter().append("circle")
-        .attr("r", 5)
-        .attr("fill", (d)=> { return this.color(d.group); })
+      .enter().append('circle')
+        .attr('r', 30)
+        .attr('fill', (d)=> { return this.color(d.group); })
         .call(d3.drag()
-            .on("start", (d)=>{return this.dragstarted(d)})
-            .on("drag", (d)=>{return this.dragged(d)})
-            .on("end", (d)=>{return this.dragended(d)}));
+            .on('start', (d) => { return this.dragstarted(d) })
+            .on('drag', (d) => { return this.dragged(d) })
+            .on('end', (d) => { return this.dragended(d) }));
 
-    this.node.append("title")
+    this.svg.append('svg:defs').append('svg:marker')
+      .attr('id', 'end-arrow')
+      .attr('viewBox', '0 -5 10 10')
+      .attr('refX', 6)
+      .attr('markerWidth', 5)
+      .attr('markerHeight', 5)
+      .attr('orient', 'auto')
+      .append('svg:path')
+        .attr('d', 'M0,-5L10,0L0,5')
+        .attr('fill', '#000');
+
+    this.node.append('title')
       .text(function(d) { return d._id; });
 
     this.simulation
       .nodes(this.studentMap.competences)
-      .on("tick", ()=>{return this.ticked()});
+      .on('tick', ()=>{return this.ticked()});
 
-    this.simulation.force("link")
+    this.simulation.force('link')
       .links(this.studentMap.connections);  
   }
   
